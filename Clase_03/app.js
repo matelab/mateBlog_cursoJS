1f16a14c506920cf0e3c31444797db27b99f71fe
jpromanonet <programming@jpromano.net>
2020-08-05 13:33:53 -0600

    Clase 03

--- Clase_03/app.js
+++ Clase_03/app.js
@@ -1,16 +1,16 @@
-// Declare variable name in the global scope
-// Everything from global Scope is avaible in everywhere
+// Declarar una variable en el scope global
+// Todo aquello en el scope global, está disponible en todos lados del archivo
 const name = "Nerd";
 
-////////////////////////////////////////// The old way to use functions!
-// Declare function
-// function scope is into the function but is not global
+////////////////////////////////////////// La vieja forma de declarar funciones!
+// Declarar una función
+// El scope de la función NO ES GLOBAL y solo está dentro de la función.
 function toUpper0(text) {
   const upperCased0 = text.toUpperCase();
   console.log(upperCased0);
 }
 
-// Function invocation
+// Asi se invoca una función
 toUpper0(name);
 
 ////////////////////////////////////////// The new way to use functions!
